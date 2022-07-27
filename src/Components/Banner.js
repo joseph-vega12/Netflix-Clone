5996f8bcff9b7e270a12a5e97938def3adda6078
Joseph Vega <josephvega@Josephs-MacBook-Pro.local>
2022-07-26 23:52:09 -0700

    fixed issue where some images were not displaying

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -20,14 +20,16 @@ function Banner() {
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
-
+  console.log(movie);
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url(
-            "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+            "https://image.tmdb.org/t/p/original/${
+              movie?.backdrop_path || movie?.poster_path
+            }"
         )`,
         backgroundPosition: "center center",
       }}
